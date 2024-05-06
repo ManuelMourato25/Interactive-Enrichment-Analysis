288ce6ebd0b772c8591d6b2e40ed85a39bad2738
ManuelMourato <manuelmourato25@gmail.com>
2024-05-06 10:06:40 +0100

    Removed version check

--- launch_app.R
+++ launch_app.R
@@ -1,16 +1,16 @@
 # Check R version
-x = readLines("http://cran.r-project.org/sources.html")
+#x = readLines("http://cran.r-project.org/sources.html")
 # the version number is in the next line of 'The latest release'
-relver = gsub("(.*R-|\\.\\d\\.tar\\.gz.*)", "", x[grep("latest release", x) + 1L])
-myver = substr(getRversion(),1,3)
+#relver = gsub("(.*R-|\\.\\d\\.tar\\.gz.*)", "", x[grep("latest release", x) + 1L])
+#myver = substr(getRversion(),1,3)
 # new version available?
 # message("Installed major version: ", myver)
 # message("Latest major version: ", relver)
-vcheck <- compareVersion(relver, myver)
-if(vcheck == 1){
-  message(R.version$version.string)
-  stop(sprintf("Please install R version %s or higher",relver))
-}
+#vcheck <- compareVersion(relver, myver)
+#if(vcheck == 1){
+#  message(R.version$version.string)
+#  stop(sprintf("Please install R version %s or higher",relver))
+#}
 # Check initial dependencies
 initial.libs <- c("shinydashboard", "shiny", "shinyjs", "shinyBS", "DT","BiocManager")
 options(install.packages.check.source = "no")
